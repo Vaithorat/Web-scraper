5a5654fb2b64324d913d4db9fb34f07bc3de0ba7
Vaibhav Thorat <vaibhavthorat141997@gmail.com>
2023-06-05 12:57:44 +0530

    guardian web scraper

--- index.js
+++ index.js
@@ -5,10 +5,25 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-axios.get("https://www.theguardian.com/uk").then((response) => {
-  const html = response.data;
-  console.log(html);
-});
+axios
+  .get("https://www.theguardian.com/uk")
+  .then((response) => {
+    const html = response.data;
+    //   console.log(html);
+    const $ = cheerio.load(html);
+    const articles = [];
+
+    $(".fc-item__title", html).each(function () {
+      const title = $(this).text();
+      const url = $(this).find("a").attr("href");
+      articles.push({
+        title,
+        url,
+      });
+    });
+    console.log(articles);
+  })
+  .catch((error) => console.log(error));
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
